c256cbd101cd861938940ef2dcc6227d6ed71556
unknown <Your Name>
2021-05-25 16:03:46 +0800

    台球

---
+++ zu.php
@@ -0,0 +1,12 @@
+<?php
+    $arr1 = [
+        "name" => "lin",
+        "age"  => "17",
+        "sex"  => "女",
+        110    => "zi",
+        120    => "xi",
+    ];
+
+    var_dump($arr1);
+    echo "\n";
+    print_r($arr1);
